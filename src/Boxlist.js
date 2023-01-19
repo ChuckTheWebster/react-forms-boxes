8ee18fa25405970968accadc09c632484397d5ae
Charlotte Webster <charlotte.shae.webster@gmail.com>
2023-01-19 15:47:55 -0500

    "update"

--- src/Boxlist.js
+++ src/Boxlist.js
@@ -1,33 +1,28 @@
 import NewBoxForm from "./NewBoxForm";
 import Box from "./Box";
 
-function Boxlist() {
-  const [boxes, setBoxes] = useState([]);
 
-    // function deleteBox(evt) {
-    //   const box = evt.target
-    //   useState({...boxes} => indexOf(box))
-    //   setBox();
-    // }
+
+function Boxlist() {
+const [boxes, setBoxes] = useState([]);
+
+  // function deleteBox(evt) {
+  //   const box = evt.target
+  //   useState({...boxes} => indexOf(box))
+  //   setBox();
+  // }
 
   function addBox(box) { // {h: ,w: ,bc: }
     let newBox = { ...box, id: uuid() }; //{h: ,w: ,bc: , id: }
     setBoxes(boxes => [...boxes, newBox]);
   } // causes re-render
 
-  const boxComponents = boxes.map(box => <Box
-    height={ box.height }
-    width={ box.width }
-    backgroundColor={ box.backgroundColor }/>);
+  }
 
-
-  return (
-    <div>
-      <NewBoxForm addBox={ addBox }/>
-      { boxComponents }
-    </div>
-  );
-
-}
+    return (
+      <NewBoxForm/>
+      { boxes.map(box => <Box height={ height } width={ width } backgroundColor={ backgroundColor } deleteBox={ deleteBox }/>) }
+    )
+  }
 
 export default Boxlist
